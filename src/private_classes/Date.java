3c6274cc29d33390b5e9cd44bf49ce334c3eb195
Eduardo Galeano <cegard689@gmail.com>
2016-10-09 13:35:34 -0500

    Date class counts with day, month and year properties as objects of private classes

--- src/private_classes/Date.java
+++ src/private_classes/Date.java
@@ -1,5 +1,47 @@
 package private_classes;
 
-public class Date {
 
+public class Date {
+	
+	
+	private Day day;
+	private Month month;
+	private Year year;
+	
+	
+	public Date(){
+		this.day = new Day();
+		this.month = new Month();
+		this.year = new Year();
+	}
+	
+	
+	public Day getDay(){
+		return this.day;
+	}
+	
+	
+	public Month getMonth(){
+		return this.month;
+	}
+	
+	
+	public Year getYear(){
+		return this.year;
+	}
+	
+	
+	private class Day{
+		
+	}
+	
+	
+	private class Month{
+		
+	}
+	
+	
+	private class Year{
+		
+	}
 }
--- src/private_classes/DatePCTest.java
+++ src/private_classes/DatePCTest.java
@@ -5,11 +5,33 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class DatePCTest {
+	
+	
 
+	private Date date = new Date();
+	
+	
 	@Test
 	public void dateClassExists() {
-		Date date = new Date();
-		assertNotNull("Date class exists", date);
+		assertNotNull("Date class exists", this.date);
+	}
+	
+	
+	@Test
+	public void dateHasADay(){
+		assertNotNull("Date class has a day property", this.date.getDay());
+	}
+	
+	
+	@Test
+	public void dateHasAMonth(){
+		assertNotNull("Date class has a month property", this.date.getMonth());
+	}
+	
+	
+	@Test
+	public void dateHasAYear(){
+		assertNotNull("Date class has a year property", this.date.getYear());
 	}
 
 }
